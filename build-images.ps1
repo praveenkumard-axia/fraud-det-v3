2eb090a9c0554c1a002d11431f2363d5265f0c3a
pravee42 <65100038+pravee42@users.noreply.github.com>
2026-02-18 22:45:33 +0530

    fix: k8s

--- build-images.ps1
+++
@@ -1,5 +0,0 @@
-# build-images.ps1: Automate building all fraud pipeline images on Windows
-# Run from repo root: .\build-images.ps1
-
-$env:NO_CACHE = ""
-powershell -ExecutionPolicy Bypass -File .\k8s\build-images.ps1
